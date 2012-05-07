f941da5f06b8637081a1f3b4c79fda30c0c2a44c
Thomas Haggett <thomas-github@haggett.org>
2012-05-07 12:54:57 +0200

    Remove zombie code...

--- liberate.m
+++ liberate.m
@@ -16,23 +16,10 @@ OSErr hooked_FSGetVolumeInfo (FSVolumeRefNum volume,
 ) {
   OSErr err = FSGetVolumeInfo(volume, volumeIndex, actualVolume, whichInfo, info, volumeName, rootDirectory);
   
-//  FILE* thelog = fopen("/tmp/output", "a");
   if(info) {
-//    char buffer[500];
-    
-//    if(rootDirectory != nil) {
-//      FSRefMakePath( rootDirectory, buffer, 500 );
-//      fprintf(thelog, "       FSGetVolumeInfo %i %i %s\n", info->filesystemID, info->signature, buffer );
-//        if( strcmp(buffer, "/Volumes/tank") == 0 ) {
         info->filesystemID = 0;
         info->signature = 18475;
-//        }
-//        fprintf(thelog, "[after] FSGetVolumeInfo %i %i %s\n", info->filesystemID, info->signature, buffer );
-//      }
-//    } else {
-//      fprintf(thelog, "No info" );
   }
-//    fclose(thelog);
   return err;
 }
 
